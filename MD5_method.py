ec384fcef2dcbff3033b1c3176f399c447bdf29e
Mostafa182003 <114989623+Mostafa182003@users.noreply.github.com>
2023-03-07 10:36:29 +0300

    Update MD5_method.py

--- MD5_method.py
+++ MD5_method.py
@@ -1,19 +1,19 @@
 import hashlib
 
-# Ввод текста
+# Text input
 text = input("Введите текст для хеширования: ")
 
-# Конвертация текста в байты
+# Convert text to bytes
 text_bytes = text.encode('utf-8')
 
-# Создание объекта MD5
+# Create an MD5 object
 md5_hash = hashlib.md5()
 
-# Обновление объекта с помощью текстовых байтов
+# Update object with text bytes
 md5_hash.update(text_bytes)
 
-# Преобразование в 16тиричная систем
+# Convert to hexadecimal systems
 hash_hex = md5_hash.hexdigest()
 
-# Вывод в консоль
+# Output to console
 print("MD5 hash текста:", hash_hex)
